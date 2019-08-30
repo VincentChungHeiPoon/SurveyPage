870c3178d321a0eb61567209b096462f785987dc
Benson Chu <bensonchu457@gmail.com>
2019-08-30 10:17:08 -0500

    Renamed txt to sql, added test data

---
+++ Survey/Survey/Data/values.sql
@@ -0,0 +1,4 @@
+insert into tblSurvey (tblSurveyID, QuestionText, QuestionDescription, Active, Modified, Created)
+Values
+(1,'What is your zip code','gather user zipcode',1,'2019-08-27 11:38:04.207','2019-08-27 11:38:04.207'),
+(2,'adsf','asdf',1,'2019-08-28 17:46:55.920','2019-08-28 17:46:55.920')
